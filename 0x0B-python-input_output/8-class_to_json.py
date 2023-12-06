ade68f24e066de49039f9dd01d25eedc73e86e73
faith481 <faithayeni481@gmail.com>
2023-12-05 16:07:07 -0800

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 """This module defines a python class-to-JSON function"""
+
 
 def class_to_json(obj):
     """Returns the dictionary description of a simple data structure for JSON serilization of an object"""
