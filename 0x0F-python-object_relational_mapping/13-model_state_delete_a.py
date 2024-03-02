70654b371c2804cfd9c135b41f277d925704f60d
faith481 <faithayeni481@gmail.com>
2024-03-02 00:30:20 -0800

    Module documented

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,15 +7,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                                                                      pool_pre_ping=True)
-            Base.metadata.create_all(engine)
-                Session = sessionmaker(bind=engine)
-                    session = Session()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+            format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-                        objects = session.query(State).filter(State.name.contains('%a'))
-                            for obj in objects:
-                                        session.delete(obj)
-                                            session.commit()
-                                                session.close()
+    objects = session.query(State).filter(State.name.contains('%a'))
+    for obj in objects:
+        session.delete(obj)
+        session.commit()
+        session.close()
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,15 +8,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                                                                      pool_pre_ping=True)
-            Session = sessionmaker(bind=engine)
-                session = Session()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+            format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-                    query = session.query(State, City).join(City, State.id == City.state_id).\
-                                    order_by(City.id)
-                                        for state, city in query:
-                                                    print("{}: ({}) {}".format(state.name, city.id, city.name))
+    query = session.query(State, City).join(City, State.id == City.state_id).\
+            order_by(City.id)
+            for state, city in query:
+                print("{}: ({}) {}".format(state.name, city.id, city.name))
 
-                                                        session.close()
+                session.close()
