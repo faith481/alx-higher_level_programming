256ada96333b6c5477a8b90bf9c29cc6f97b0775
faith481 <faithayeni481@gmail.com>
2024-03-02 00:17:55 -0800

    Module documented

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,13 +7,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                                                                      pool_pre_ping=True)
-            Base.metadata.create_all(engine)
-                Session = sessionmaker(bind=engine)
-                    session = Session()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+            format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-                        for states_id, states_name in session.query(State.id,
-                                                                                State.name).order_by(State.id):
-                                    print("{}: {}".format(states_id, states_name))
+    for states_id, states_name in session.query(State.id,
+            State.name).order_by(State.id):
+        print("{}: {}".format(states_id, states_name))
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,11 +7,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                                                                      pool_pre_ping=True)
-            Session = sessionmaker(bind=engine)
-                session = Session()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+            format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-                    for states in session.query(State).filter(State.name.contains('%a')):
-                                print("{}: {}".format(states.id, states.name))
+    for states in session.query(State).filter(State.name.contains('%a')):
+        print("{}: {}".format(states.id, states.name))
