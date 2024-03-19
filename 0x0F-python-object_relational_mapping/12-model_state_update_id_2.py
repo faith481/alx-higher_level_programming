b3691e7faf42b72a9968abf63d1e28a2aa5d01d5
faith481 <faithayeni481@gmail.com>
2024-03-19 05:51:20 -0700

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -2,18 +2,20 @@
 """Script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
 """
 import sys
-from model_state import Base, State
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+from model_state import Base, State
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-            format(sys.argv[1], sys.argv[2], sys.argv[3]),
-            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    session.query(State).filter(State.id == "2").\
-            update({State.name: "New Mexico"})
-    session.commit()
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        passwd = sys.argv[2]
+        dbname = sys.argv[3]
+        dburl = f"mysql://{user}:{passwd}@localhost:3306/{dbname}"
+        dbengine = create_engine(dburl)
+        Base.metadata.create_all(dbengine)
+        dbsession = sessionmaker(bind=dbengine)()
+        dbsession.query(State).filter(State.id == 2).update(
+                {State.name: 'New Mexico'}, synchronize_session=False)
+        dbsession.commit()
