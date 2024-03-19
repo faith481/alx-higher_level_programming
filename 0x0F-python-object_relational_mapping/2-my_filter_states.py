14d20fef2f63b53bef7e1a799e54b3671c5d6ce2
faith481 <faithayeni481@gmail.com>
2024-03-19 05:24:14 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,22 +6,27 @@ where `name` matches the argument
 from the database `hbtn_0e_0_usa`.
 """
 
-import MySQLdb as db
-from sys import argv
+import MySQLdb
+import sys
+
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db_connect = db.connect(host="localhost", port=3306,
-            user=argv[1], passwd=argv[2], db=argv[3])
-    db_cursor = db_connect.cursor()
-
-    db_cursor.execute(
-    "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-            states.id ASC".format(argv[4]))
-    rows_selected = db_cursor.fetchall()
-
-    for row in rows_selected:
-        print(row)
+    if len(sys.argv) >= 5:
+        dbconn = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
+        )
+        dbcur = dbconn.cursor()
+        stname = sys.argv[4]
+        dbqry = (
+            'SELECT * FROM states WHERE CAST(name AS BINARY) LIKE ' +
+            'CAST("{}" AS BINARY) ORDER BY id ASC;'.format(stname)
+        )
+        dbcur.execute(dbqry)
+        qryres = dbcur.fetchall()
+        for row in qryres:
+            print(row)
+        dbconn.close()
