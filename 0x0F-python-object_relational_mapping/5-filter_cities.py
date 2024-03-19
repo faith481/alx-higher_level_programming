5a8b3ca07d418a0172c419ac3ec63b4fcd3a0c05
faith481 <faithayeni481@gmail.com>
2024-03-19 05:31:31 -0700

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,30 +6,24 @@ import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-
-    db = MySQLdb.connect(
-            host='localhost',
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
-            )
-    state_name = sys.argv[4]
-    cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities\
-            LEFT JOIN states\
-            ON states.id = cities.state_id\
-            WHERE states.name LIKE BINARY (%s) ORDER BY cities.id ASC",
-            (state_name,))
-    table = cur.fetchall()
-
-    end_str = ""
-    str_cities = ""
-    for row in table:
-        str_cities = str_cities + end_str + row[0]
-        end_str = ", "
-
-        print(str_cities)
-        cur.close()
-        db.close()
+if __name__ == '__main__':
+    if len(sys.argv) >= 5:
+        dbconn = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
+        )
+        dbcur = dbconn.cursor()
+        stname = sys.argv[4]
+        dbqry = (
+            'SELECT cities.name FROM cities ' +
+            'INNER JOIN states ON cities.state_id = states.id ' +
+            'WHERE CAST(states.name AS BINARY) = %s ' +
+            'ORDER BY cities.id ASC;'
+        )
+        dbcur.execute(dbqry, (stname,))
+        qryres = dbcur.fetchall()
+        print(', '.join(map(lambda cty: cty[0], qryres)))
+        dbconn.close()
