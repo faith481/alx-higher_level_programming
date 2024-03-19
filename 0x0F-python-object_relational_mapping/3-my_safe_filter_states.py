1d3ad87488ebae27f3d0e0278599beb3c78e6032
faith481 <faithayeni481@gmail.com>
2024-03-19 05:26:39 -0700

    SQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,61 +3,27 @@
 script that takes in arguments and displays all values in the
 states table of hbtn_0e_0_usa where name matches the argument.
 """
-
-
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-
-        db = MySQLdb.connect(
-                        host='localhost',
-                                user=sys.argv[1],
-                                        passwd=sys.argv[2],
-                                                db=sys.argv[3],
-                                                        port=3306
-                                                                )
-            state_name = sys.argv[4]
-
-                cur = db.cursor()
-                    cur.execute("SELECT * FROM states WHERE name = (%s)\
-                                ORDER BY states.id ASC", (state_name,))
-                        table = cur.fetchall()
-
-                            for row in table:
-                                        print(row)
-
-                                            cur.close()
-                                                db.close()#!/usr/bin/python3
-                                                """
-                                                script that takes in arguments and displays all values in the
-                                                states table of hbtn_0e_0_usa where name matches the argument.
-                                                """
-
-
-                                                import MySQLdb
-                                                import sys
-
-
-                                                if __name__ == "__main__":
-
-                                                        db = MySQLdb.connect(
-                                                                        host='localhost',
-                                                                                user=sys.argv[1],
-                                                                                        passwd=sys.argv[2],
-                                                                                                db=sys.argv[3],
-                                                                                                        port=3306
-                                                                                                                )
-                                                            state_name = sys.argv[4]
-
-                                                                cur = db.cursor()
-                                                                    cur.execute("SELECT * FROM states WHERE name = (%s)\
-                                                                                ORDER BY states.id ASC", (state_name,))
-                                                                        table = cur.fetchall()
-
-                                                                            for row in table:
-                                                                                        print(row)
-
-                                                                                            cur.close()
-                                                                                                db.close()
+if __name__ == '__main__':
+    if len(sys.argv) >= 5:
+        dbconn = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
+        )
+        dbcur = dbconn.cursor()
+        stname = sys.argv[4]
+        dbqry = (
+            'SELECT * FROM states WHERE CAST(name AS BINARY) ' +
+            'LIKE %s ORDER BY id ASC;'
+        )
+        dbcur.execute(dbqry, ('%' + stname + '%',))
+        qryres = dbcur.fetchall()
+        for row in qryres:
+            print(row)
+        dbconn.close()
